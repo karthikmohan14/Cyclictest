79fad2de9306850dbd7d076778e61463bed64563
karthikmohan14 <karthikmkochi@gmail.com>
2020-02-04 15:56:47 +0000

    updated shell script

--- latencyplot.sh
+++ latencyplot.sh
@@ -21,13 +21,13 @@ done
 
 # 6. Create plot command header
 echo -n -e "set title \"Latency plot\"\n\
-set terminal png\n\
+set terminal svg\n\
 set xlabel \"Latency (us), max $max us\"\n\
 set logscale y\n\
 set xrange [0:400]\n\
 set yrange [0.8:*]\n\
 set ylabel \"Number of latency samples\"\n\
-set output \"plot.png\"\n\
+set output \"plot.svg\"\n\
 plot " >plotcmd
 
 # 7. Append plot command data references
